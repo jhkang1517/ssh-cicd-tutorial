28a913e24b39a16575b09446ccd3f600c7d9acfa
jhkang1517 <jhkang1517@gmail.com>
2022-11-27 21:36:37 +0900

    initial commit
    Signed-off-by: jhkang1517 <jhkang1517@gmail.com>

---
+++ src/main/java/com/blackswitch/sshcicdtutorial/SshCicdTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.blackswitch.sshcicdtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SshCicdTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SshCicdTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackswitch/sshcicdtutorial/SshCicdTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackswitch.sshcicdtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshCicdTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
